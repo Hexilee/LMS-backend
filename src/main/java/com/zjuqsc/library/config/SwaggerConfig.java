8a7dda6014e70c715b2010fe31be9e6a8f0bc2a6
Hexilee <hexileee@gmail.com>
2018-05-02 19:25:00 +0800

    complete swagger config

---
+++ src/main/java/com/zjuqsc/library/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.zjuqsc.library.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@ConfigurationProperties(prefix = "swagger")
+public class SwaggerConfig {
+    @Data
+    public static class Info {
+        private String version;
+        private String title;
+        private String description;
+        private String license;
+        private String licenseUrl;
+        private String contactName;
+        private String contactURL;
+        private String contactEmail;
+    }
+
+    @NotNull private Info info;
+
+}
---
+++ src/main/java/com/zjuqsc/library/configuration/Swagger2Configure.java
@@ -0,0 +1,53 @@
+package com.zjuqsc.library.configuration;
+
+import com.zjuqsc.library.config.SwaggerConfig;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@EnableConfigurationProperties(SwaggerConfig.class)
+public class Swagger2Configure {
+    private SwaggerConfig swagger;
+
+    public Swagger2Configure(SwaggerConfig swagger) {
+        this.swagger = swagger;
+    }
+
+    @Bean
+    public Docket documentation() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.zjuqsc.library"))
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/api");
+    }
+
+    private ApiInfo apiInfo() {
+        SwaggerConfig.Info info = swagger.getInfo();
+        return new ApiInfoBuilder()
+                .version(info.getVersion())
+                .title(info.getTitle())
+                .description(info.getDescription())
+                .license(info.getLicense())
+                .licenseUrl(info.getLicenseUrl())
+                .contact(new Contact(
+                        info.getContactName(),
+                        info.getContactURL(),
+                        info.getContactEmail()
+                ))
+                .build();
+    }
+}
