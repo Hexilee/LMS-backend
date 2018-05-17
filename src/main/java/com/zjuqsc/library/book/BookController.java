f5a6ed8f87f225c4fbcd20d0c7a4a10cc964dc64
Hexilee <hexileee@gmail.com>
2018-05-17 12:37:41 +0800

    remove log in BookController.register

--- src/main/java/com/zjuqsc/library/book/BookController.java
+++ src/main/java/com/zjuqsc/library/book/BookController.java
@@ -1,9 +1,7 @@
 package com.zjuqsc.library.book;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zjuqsc.library.advice.dto.ErrorInfoDto;
 import com.zjuqsc.library.auth.AuthConstant;
-import com.zjuqsc.library.auth.AuthUtils;
 import com.zjuqsc.library.book.dto.BookDto;
 import com.zjuqsc.library.exception.ResourceKeyNotExistException;
 import io.swagger.annotations.*;
@@ -13,10 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Li Chenxi
@@ -29,12 +24,6 @@ public class BookController {
     private static final String BCID_KEY = "bcid";
     private static final String PAGE_ID_KEY = "pages";
     private BookService bookService;
-
-    @Autowired
-    private AuthUtils authUtils;
-    @Autowired
-    private ObjectMapper jacksonObjectMapper;
-
 
     @Autowired
     public BookController(BookService bookService) {
@@ -64,19 +53,8 @@ public class BookController {
     @PostMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public BookDto register(
-            HttpServletRequest request,
             @RequestParam(value = BCID_KEY) Integer bcid
     ) throws ResourceKeyNotExistException {
-        authUtils.getToken(request)
-                .map(token ->
-                        authUtils.createUserInfo(token).get()
-                )
-                .ifPresent(userInfoDto -> {
-                            for (GrantedAuthority grantedAuthority : userInfoDto.getAuthorities()) {
-                                log.info("ROLE: " + grantedAuthority.getAuthority());
-                            }
-                        }
-                );
         return bookService.register(bcid)
                 .orElseThrow(() -> new ResourceKeyNotExistException(BCID_KEY, bcid));
     }
--- src/main/java/com/zjuqsc/library/book/BookUtils.java
+++ src/main/java/com/zjuqsc/library/book/BookUtils.java
@@ -2,12 +2,13 @@ package com.zjuqsc.library.book;
 
 import com.zjuqsc.library.book.dto.BookDto;
 import com.zjuqsc.library.entity.Book;
+import lombok.val;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BookUtils {
     public BookDto createBookDto(Book book) {
-        BookDto bookDto = new BookDto();
+        val bookDto = new BookDto();
         bookDto.setBid(book.getBid());
         bookDto.setBcid(book.getBookClass().getBcid());
         bookDto.setAccessible(book.isAccessible());
